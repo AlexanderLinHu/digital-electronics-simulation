a98099f1fa6b4950455edcacee53b53e0d82ba5d
UsernameIsTaken88 <alexanderlinhu8888@gmail.com>
2025-01-03 13:03:41 -0500

    Token.Category start

--- src/main/sim/compiler/lexer/Token.java
+++ src/main/sim/compiler/lexer/Token.java
@@ -1,5 +1,22 @@
 package sim.compiler.lexer;
-
+// cspell:disable
 public class Token {
-    
+
+    public enum Category {
+
+        //>> Delimiters
+        LBRA,           // '{'  // Left brace
+        RBRA,           // '}'  // Right brace
+        LPAR,           // '('  // Left parenthesis
+        RPAR,           // ')'  // Right parenthesis
+        LSBR,           // '['  // Left square brace
+        RSBR,           // ']'  // Right square brace
+        ASSIGN,         // '='
+        MINUS,          // '-'
+        COMMA,          // ','
+        ARROW,          // '-->'
+        COLON,          // ':'
+        DOUBLE_COLON,   // '::' // Output bus aliasing
+
+    }
 }
